476d5532453e66db2e2a72ac64e0814e8db3809d
Mixx <zeltins.miks24@gmail.com>
2022-04-22 18:35:31 +0300

    POPmap

--- app/src/main/java/com/example/popmap/MainActivity.java
+++ app/src/main/java/com/example/popmap/MainActivity.java
@@ -10,8 +10,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        System.out.print("Kurwa");
-        System.out.println("Šašulis mazulis");
-        System.out.println("mama mia");
+        //TODO pamats čatam
+
     }
 }
